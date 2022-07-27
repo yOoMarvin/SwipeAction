df7b10995ba72a1f33422eb9068af8be000872f5
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-07-27 21:03:49 +0200

    Initial Commit

---
+++ SwipeAction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipeAction
+//
+//  Created by Marvin Messenzehl on 27.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipeAction/SwipeActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionApp.swift
+//  SwipeAction
+//
+//  Created by Marvin Messenzehl on 27.07.22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
